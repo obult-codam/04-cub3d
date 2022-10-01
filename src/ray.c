72aa0b9b6a21c25b27a85ef0b988178902d603f4
Oswin Bult <oswin.bult@live.nl>
2022-10-01 23:05:51 +0200

    fixed the issue that it would add one more tile of lenght when between 90 and 270 degrees

--- src/ray.c
+++ src/ray.c
@@ -6,7 +6,7 @@
 /*   By: obult <obult@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/28 17:25:47 by obult         #+#    #+#                 */
-/*   Updated: 2022/10/01 19:24:50 by oswin         ########   odam.nl         */
+/*   Updated: 2022/10/01 23:04:13 by oswin         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,9 +68,12 @@ double	max_x_dist(t_data data, double angle, double x, double y)
 
 	y_dist = calc_distance(y, angle);
 	x_dist = tan(angle) * y_dist;
-	if ((int)round(x_dist + x) > data.x_max || (int)round(x_dist + x) < 0)
+	if ((int)round(x_dist + x) > data.x_max || (int)round(x_dist + x) < 1)
 		return (ERROR);
-	if (data.map[(int)round(y + y_dist)][(int)round(x + x_dist)] != '0')
+	if (angle < 0.5 * PI || angle > 1.5 * PI)
+		if (data.map[(int)round(y + y_dist)][(int)round(x + x_dist)] != '0')
+			return (x_dist);
+	if (data.map[(int)round(y + y_dist) - 1][(int)round(x + x_dist) - 1] != '0')
 		return (x_dist);
 	result = max_x_dist(data, angle, x + x_dist, y + y_dist);
 	if (result == ERROR)
@@ -100,7 +103,7 @@ int	main(void)
 
 	printf("next should be -0.577350\n");
 	printf("calc : %f\n", max_x_dist(data, PI / 6 * 7, 2, 2));
-	printf("this is incorrect because it now runs twice as far\n");
+	printf("this is incorrect because it now runs twice as far, not anymore since I fixed it\n");
 
 	printf("next should be ERROR / INT_MAX\n");
 	printf("calc : %f\n", max_x_dist(data, PI / 2, 2, 2));
